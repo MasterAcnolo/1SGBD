83d1ac2a27500b665c7537469c18b3f8274792d1
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-09 09:00:32 +0100

    Add: New historical narratives for America, Asia, and Europe

---
+++ america.sql
@@ -0,0 +1,7 @@
+-- Les Canadiens ont annexé les états américains du Michigan, du Wisconsin, de l'Illinois, de l'Alaska et de Washington.
+
+-- Les anciennes provinces du Québec et de l'Ontario forment désormais un état souverain, dont la capitale est Montréal.
+
+-- Les États-Uniens ont conquis de nouvelles terres au sud : ils occupent désormais le Mexique, Cuba, le Venezuela et la Colombie.
+
+-- En réaction, tous les pays hispanophones au sud de la Colombie et du Brésil forment désormais un unique pays : les Nouveaux États-Unis d'Amérique.
---
+++ europa.sql
@@ -0,0 +1,7 @@
+-- En France, la Bretagne a colonisé la Normandie et forme désormais un nouvel état souverain, dont la langue officielle est le breton et dont la capitale est Rennes.
+
+-- La région Centre a déclaré son indépendance, s’est baptisée « République du Val de Loire » et a fait de Tours sa capitale.
+
+-- Lyon, Caen et Lille sont désormais trois cités-États indépendantes.
+
+-- Le Liechtenstein a annexé la Suisse et la Lombardie. 
