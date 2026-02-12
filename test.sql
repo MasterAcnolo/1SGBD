de543de1e87c47df5e29feafecbab56c5fb22fd1
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-12 14:32:00 +0100

    Fix: Clean Test File

--- test.sql
+++ test.sql
@@ -1,8 +1,3 @@
 use NewWorld;
 
--- SELECT Name, Code, Population from Country WHERE Name LIKE "Canada"
--- SELECT * FROM Country
-SELECT * FROM Country WHERE Name Like 'Québec-Ontario'
-
--- AVANT: 31147000
--- APRES: 39217685; 
+DESCRIBE Country;
