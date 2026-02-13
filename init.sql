8fff2f385853b78b4c5ca3b08b91bbfa38af8b6a
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-13 10:11:50 +0100

    Fix: Clean Init.sql files

--- init.sql
+++ init.sql
@@ -3,8 +3,6 @@ CREATE DATABASE IF NOT EXISTS NewWorld;
 USE NewWorld;
 
 -- reset tables
-DROP TABLE IF EXISTS ARMEE;
-DROP TABLE IF EXISTS GUERRE;
 DROP TABLE IF EXISTS CountryLanguage;
 DROP TABLE IF EXISTS City;
 DROP TABLE IF EXISTS Country;
@@ -18,29 +16,3 @@ INSERT INTO City SELECT * FROM world.City;
 
 CREATE TABLE CountryLanguage LIKE world.CountryLanguage;
 INSERT INTO CountryLanguage SELECT * FROM world.CountryLanguage;
-
--- tables du jeu
-CREATE TABLE GUERRE(
-    id INT NOT NULL AUTO_INCREMENT,
-    code_pays_attaquant CHAR(3) NOT NULL,
-    code_pays_defenseur CHAR(3) NOT NULL,
-    estActive BOOLEAN DEFAULT TRUE,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE ARMEE(
-    id INT NOT NULL AUTO_INCREMENT,
-    code_pays CHAR(3) NOT NULL,
-    nombre_soldats INT DEFAULT 0,
-    nombre_chars INT DEFAULT 0,
-    nombre_avions INT DEFAULT 0,
-    PRIMARY KEY (id)
-);
-
--- FK
-ALTER TABLE GUERRE
-ADD CONSTRAINT fk_guerre_attaquant FOREIGN KEY (code_pays_attaquant) REFERENCES Country(Code),
-ADD CONSTRAINT fk_guerre_defenseur FOREIGN KEY (code_pays_defenseur) REFERENCES Country(Code);
-
-ALTER TABLE ARMEE
-ADD CONSTRAINT fk_armee_code_pays FOREIGN KEY (code_pays) REFERENCES Country(Code);
