f7a89b4328a8a3be1269a32cfb3f181d0c7a65c2
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-13 10:12:41 +0100

    Fix: Check.sql files and clean useless element

--- utils/check.sql
+++ utils/check.sql
@@ -1,15 +1,6 @@
-USE world_sim;
+USE newWorld;
 
 -- Tables principales World
 DESCRIBE Country;
 DESCRIBE City;
 DESCRIBE CountryLanguage;
-
--- Tables du jeu
-DESCRIBE GUERRE;
-DESCRIBE ARMEE;
-
--- Clés et contraintes
-SHOW INDEX FROM Country;
-SHOW CREATE TABLE GUERRE;
-SHOW CREATE TABLE ARMEE;
